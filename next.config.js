c9f21f5a09e81f8bb0f76c076ca783ae857dfe2a
Sergio <deguzmansergei@gmail.com>
2023-02-22 23:51:14 -0700

    navbar, banner, rows

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "image.tmdb.org", pathname: "/**" },
+    ],
+  },
+};
