20d41e6780f92310ae164ff45e8bc18e4cb19bc8
Sergio <deguzmansergei@gmail.com>
2023-02-23 00:14:48 -0700

    row finished + styling

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { Movie } from "../typings";
 import Thumbnail from "./Thumbnail";
+import React, { useRef, useState } from "react";
 
 interface Props {
   title: string;
@@ -8,19 +9,44 @@ interface Props {
 }
 
 function Row({ title, movies }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction: "left" | "right") => {
+    setIsMoved(true);
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
         {title}
       </h2>
       <div className="group relative md:-ml-2">
-        <ChevronLeftIcon className="rowChevron left-2" />
-        <div className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+        <ChevronLeftIcon
+          className={`rowChevron left-2 ${!isMoved && "hidden"}`}
+          onClick={() => handleClick("left")}
+        />
+        <div
+          ref={rowRef}
+          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
           {movies?.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon className="rowChevron right-2" />
+        <ChevronRightIcon
+          className="rowChevron right-2"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
