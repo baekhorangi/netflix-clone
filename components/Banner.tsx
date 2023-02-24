3b502382adf25c8d787c83bf558a52c9d9467aec
Sergio <deguzmansergei@gmail.com>
2023-02-23 18:58:06 -0700

    lineclamp, banner modal fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -39,7 +39,7 @@ function Banner({ netflixOriginals }: Props) {
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl line-clamp-2 md:line-clamp-3">
         {movie?.overview}
       </p>
       <div className="flex space-x-3">
@@ -51,7 +51,7 @@ function Banner({ netflixOriginals }: Props) {
         <button
           className="bannerButton bg-[gray]/70"
           onClick={() => {
-            setCurrentMovie(movie);
+            setCurrentMovie({ ...movie, media_type: "tv" });
             setShowModal(true);
           }}>
           <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" /> More Info
